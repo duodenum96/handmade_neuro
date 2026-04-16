13dc3577f1efefff6193b60b79a47a54a732dad9
Yasir Catal <catalyasir@gmail.com>
2026-04-15 22:51:02 -0400

    initial commit, basic organization

---
+++ io/io_1subj.jl
@@ -0,0 +1,7 @@
+import MAT as mat
+
+root = "/home/duodenum/Work/handmade_neuro/"
+data_path = "ds000117/sub-01/ses-meg/meg/sub-01_ses-meg_task-facerecognition_run-01_meg.fif"
+mat.matread(abspath(joinpath(root, data_path)))
+
+
