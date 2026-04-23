54ac5bffb4cc2c4c755d13c5f94fcd8e57b7db7a
Yasir Catal <catalyasir@gmail.com>
2026-04-22 20:47:30 -0400

    initialize filters file

--- constants.jl
+++ constants.jl
@@ -1,4 +1,3 @@
 const project_path = @__DIR__
 const raw_path = joinpath(project_path, "data", "raw_data", "P3")
 
-
---
+++ filtering/filters.jl
@@ -0,0 +1,9 @@
+"""
+Every linear filter has an _impulse response_, a _step response_ and a 
+_frequency response_. Each one contains the complete information about the 
+filter. 
+
+Simplest filter implementation: convolve the input signal with the 
+digital filter's impulse response. 
+"""
+
